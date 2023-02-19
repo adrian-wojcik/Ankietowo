0c444848aade034e9364f23e0fba52a2181fdbcc
Adrian <adrian90909@ont.pl>
2023-02-19 11:01:27 +0100

    Adding models (questionnaire and user) with migration and adding app into settings

--- Ankietowo/polls/models.py
+++ Ankietowo/polls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField, Model, TextField
 
-# Create your models here.
+
+
+class questionnaire(models.Model):
+    reference = IntegerField(primary_key="ref")
+    question_text = models.CharField(max_length=400)
+    publicy_date = models.DateTimeField('Data dodania ankiety')
+    answer_1 = models.CharField(max_length=200)
+    answer_2 = models.CharField(max_length=200)
+    answer_3 = models.CharField(max_length=200)
+    answer_4 = models.CharField(max_length=200)
+
+
+class user(models.Model):
+    name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=30, primary_key="email")
+    date_of_birth = models.DateField('Data Urodzenia')
+
