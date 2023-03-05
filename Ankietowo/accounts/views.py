02ffb10a101ab318f7efd03530a8bad37c06faf2
Mateusz Dadaś <m.dadas@bankier.pl>
2023-03-05 13:08:31 +0100

    sprzątanie

--- Ankietowo/accounts/views.py
+++ Ankietowo/accounts/views.py
@@ -1,30 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
-# Create your views here.
-
-
+@login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
-
+    created = request.GET.get("created", False)
+    return render(request, "accounts/profile.html", {"created": created})
 
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
+
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=password)
             login(request, user)
-            return redirect('login')
+            return redirect(reverse("accounts:profile") + "?created=True")
     else:
         form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
+    return render(request, "registration/register.html", {"form": form})
